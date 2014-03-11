c9dc6550f9468493eda72e9f1af80940e4cd3095
feeb <feefee@gmail.com>
2014-03-11 16:13:03 -0700

    patrol server

---
+++ server/AssetManager/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AssetManager::Application.config.secret_key_base = '87a25b1de7e81ef7043998a3747f1fab099be1a37443ea74c1121e16d7633212522a1829ddbdcd77b548374c925b0c80af30447dfc040159ced86e9775371966'
---
+++ server/AssetManager/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AssetManager::Application.config.session_store :cookie_store, key: '_AssetManager_session'
