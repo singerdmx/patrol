43d581db00ac8d02ae19f80967045a7ec60b2595
Xin Yao <singerdmx@gmail.com>
2014-03-11 15:25:36 -0700

    Mobile app initial commit

---
+++ mobile/app/src/main/java/com/mbrite/patrol/app/MainActivity.java
@@ -0,0 +1,25 @@
+package com.mbrite.patrol.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "started");
+        setContentView(R.layout.activity_main);
+    }
+
+    // Called to lazily initialize the action bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
