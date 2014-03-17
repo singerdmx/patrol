ec11f807e42c60c9e08adbebcc35ea0817892129
Yin Ding <dingyin@gmail.com>
2014-03-17 01:11:24 -0700

    web: add user/admin sign in pages

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,9 +1,12 @@
 Blog::Application.routes.draw do
-
-
-
- # root "welcome#index"
   resources :assets,   :defaults => { :format => 'json' }
   resources :check_routes,   :defaults => { :format => 'json' }
   resources :check_points,   :defaults => { :format => 'json' }
+
+  devise_for :admins
+  devise_for :users
+
+  root "welcome#index"
+
+  resources :admins, only: :index
 end
