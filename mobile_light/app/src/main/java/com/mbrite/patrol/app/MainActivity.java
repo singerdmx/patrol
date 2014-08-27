0b407e399448de4475670073693f59f5da81ea43
Xin Yao <xyao@vmware.com>
2014-08-26 17:03:01 -0700

    Enable scan barcode functionality

--- mobile_light/app/src/main/java/com/mbrite/patrol/app/MainActivity.java
+++ mobile_light/app/src/main/java/com/mbrite/patrol/app/MainActivity.java
@@ -4,7 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.mbrite.patrol.common.Tracker;
 import com.mbrite.patrol.common.Utils;
 
 
@@ -15,6 +21,8 @@ public class MainActivity extends ParentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setWindowTitle(R.string.patrol);
+
+        setupScanButton();
     }
 
 
@@ -39,4 +47,41 @@ public class MainActivity extends ParentActivity {
                 return false;
         }
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        Tracker.INSTANCE.startedScan = false;
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (resultCode == RESULT_OK) {
+            try {
+                Toast.makeText(
+                        this,
+                        scanResult.getContents(),
+                        Toast.LENGTH_LONG)
+                        .show();
+            } catch (Exception ex) {
+                Toast.makeText(
+                        this,
+                        String.format(getString(R.string.error_of), ex.getLocalizedMessage()),
+                        Toast.LENGTH_LONG)
+                        .show();
+            }
+        } else {
+            Toast.makeText(
+                    this,
+                    getString(R.string.not_scanned),
+                    Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
+    private void setupScanButton() {
+        TextView scanButton = (TextView) findViewById(R.id.scan_button);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Tracker.INSTANCE.startScan(MainActivity.this);
+            }
+        });
+    }
+
 }
