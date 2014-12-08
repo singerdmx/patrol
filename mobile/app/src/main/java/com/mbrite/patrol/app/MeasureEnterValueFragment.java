b0515657766cdca858ed546a6a7aaa57005259f6
Xin Yao <xyao@vmware.com>
2014-12-07 22:49:55 -0800

    Update "介于min和max之间" to be .2f

--- mobile/app/src/main/java/com/mbrite/patrol/app/MeasureEnterValueFragment.java
+++ mobile/app/src/main/java/com/mbrite/patrol/app/MeasureEnterValueFragment.java
@@ -113,7 +113,7 @@ public class MeasureEnterValueFragment extends PointsFragment {
             high = Utils.getDouble(point.choice.get(2));
             max = Utils.getDouble(point.choice.get(3));
             if (min != null && max != null) {
-                rangeDisplayValue = String.format("介于%1$,.0f和%2$,.0f之间", min, max);
+                rangeDisplayValue = String.format("介于%1$,.2f和%2$,.2f之间", min, max);
             } else if (min != null) {
                 rangeDisplayValue = String.format("大于%1$,.0f", min);
             } else if (max != null) {
