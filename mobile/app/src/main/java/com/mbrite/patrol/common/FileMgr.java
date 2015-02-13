7dcfcd655331eba4b3f1198c417de1d8c34a64b3
Xin Yao <xyao@vmware.com>
2015-02-13 09:07:55 -0800

    Fix taking pic to be full size instead of thumbnail

--- mobile/app/src/main/java/com/mbrite/patrol/common/FileMgr.java
+++ mobile/app/src/main/java/com/mbrite/patrol/common/FileMgr.java
@@ -3,10 +3,36 @@ package com.mbrite.patrol.common;
 import android.app.Activity;
 import android.content.Context;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class FileMgr {
+
+    public static String getFullPath(Activity activity, String relativePath) {
+        return activity.getFileStreamPath(relativePath).getAbsolutePath();
+    }
+
+    public static void copy(Activity activity, File src, File dst) throws IOException {
+        InputStream in = new FileInputStream(src);
+        OutputStream out = new FileOutputStream(dst);
+        try {
+            // Transfer bytes from in to out
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+        } catch (Exception ex) {
+            Utils.showErrorPopupWindow(activity, ex);
+        } finally {
+            in.close();
+            out.close();
+        }
+    }
 
     public static boolean exists(Activity activity, String fileName) {
         return activity.getFileStreamPath(fileName).exists();
